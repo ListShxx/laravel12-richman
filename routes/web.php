57958de46160e470f12b857d3eac5a61f1a97308
ListShxx <phongsakon.sri@vru.ac.th>
2026-09-12 16:56:28 +0700

    12/09/68

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+// use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProfileController;
+use App\Models\Product;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -21,19 +24,27 @@ require __DIR__ . '/auth.php';
 Route::get("/homepage", function () {
     return "<h1>This is home page</h1>";
 });
+
 Route::get("/blog/{id}", function ($id) {
     return "<h1>This is blog page : {$id} </h1>";
 });
+
+Route::get("/category/{a?}", function ($a = "mobile") {
+    return "<h1>This is category page : {$a} </h1>";
+});
+
 Route::get("/hello", function () {
     return view("hello");
 });
+
 Route::get('/greeting', function () {
 
-    $name = 'James';
-    $last_name = 'Mars';
+	$name = 'Tayanon';
+$last_name = 'Hakhun';
 
-    return view('greeting', compact('name', 'last_name'));
+return view('greeting', compact('name','last_name') );
 });
+
 Route::get("/gallery", function () {
     $ant = "https://cdn3.movieweb.com/i/article/Oi0Q2edcVVhs4p1UivwyyseezFkHsq/1107:50/Ant-Man-3-Talks-Michael-Douglas-Update.jpg";
     $bird = "https://images.indianexpress.com/2021/03/falcon-anthony-mackie-1200.jpg";
@@ -43,6 +54,7 @@ Route::get("/gallery", function () {
 
     return view("test/index", compact("ant", "bird", "cat", "god", "spider"));
 });
+
 Route::get("/gallery/ant", function () {
     $ant = "https://cdn3.movieweb.com/i/article/Oi0Q2edcVVhs4p1UivwyyseezFkHsq/1107:50/Ant-Man-3-Talks-Michael-Douglas-Update.jpg";
     return view("test/ant", compact("ant"));
@@ -58,16 +70,16 @@ Route::get("/gallery/cat", function () {
     return view("test/cat", compact("cat"));
 });
 
-Route::get("/teacher", function () {
-    return view("teacher");
+Route::get("/teacher" , function (){
+	return view("teacher");
 });
 
-Route::get("/student", function () {
-    return view("student");
+Route::get("/student" , function (){
+	return view("student");
 });
 
-Route::get("/theme", function () {
-    return view("theme");
+Route::get("/theme" , function (){
+	return view("theme");
 });
 
 Route::get('/active/index', function () {
@@ -92,3 +104,99 @@ Route::get('/active/blog', function () {
 Route::get('/active/contact', function () {
     return view('active/contact');
 })->name('contact');
+
+
+Route::get('/test',function(){
+    return view('test');
+})->name('test');
+
+
+
+Route::get('/coronavirus',function(){
+    $reports = [
+        (object) ["country"=>"Thailand" , "date"=>"2020-04-19" , "total"=>"2765", "active"=>"790"  , "death"=>"47", "recovered"=>"1928"],
+        (object) ["country"=>"Thailand" , "date"=>"2020-04-18" , "total"=>"2733", "active"=>"899"  , "death"=>"47", "recovered"=>"1787"],
+        (object) ["country"=>"Thailand" , "date"=>"2020-04-17" , "total"=>"2700", "active"=>"964"  , "death"=>"47", "recovered"=>"1689"],
+        (object) ["country"=>"China" , "date"=>"2020-04-16" , "total"=>"2672", "active"=>"1033" , "death"=>"46", "recovered"=>"1593"],
+        (object) ["country"=>"China" , "date"=>"2020-04-15" , "total"=>"2643", "active"=>"1103" , "death"=>"43", "recovered"=>"1497"],
+    ];
+    return view("coronavirus", compact("reports") );
+})->name('coronavirus');
+
+// Route::get('/category/sport', [CategoryController::class, "sport"]);
+// Route::get('/category/politic', [CategoryController::class, "politic"]);
+// Route::get('/category/entertain', [CategoryController::class, "entertain"]);
+// Route::get('/category/auto', [CategoryController::class, "auto"]);
+
+
+// use App\Models\Product;
+// use Illuminate\Support\Facades\DB;
+
+Route::get('query/sql', function () {
+    $products = DB::select("SELECT * FROM products");
+    // $products = DB::select("SELECT * FROM products WHERE price > 100");
+    return view('query-test', compact('products'));
+});
+
+Route::get('query/builder', function () {
+    $products = DB::table('products')->get();
+    // $products = DB::table('products')->where('price', '>', 100)->get();
+    return view('query-test', compact('products'));
+});
+
+Route::get('query/orm', function () {
+    $products = Product::get();
+    // $products = Product::where('price', '>', 100)->get();
+    return view('query-test', compact('products'));
+});
+
+Route::get('barchart', function () {    
+    return view('barchart');
+})->name('barchart');
+
+Route::get('product-index', function () {
+    $products = Product::get();
+    return view('query-test', compact('products'));
+})->name("product.index");
+
+
+Route::get('product-form', function () {    
+    return view('product-form');
+})->name("product.form");
+
+Route::post('/product-submit', function (Request $request) {    
+    $data = $request->validate([
+        'name' => 'required|string|max:255',
+        'description' => 'required|string',
+        'price' => 'required|numeric|min:0',
+        'image' => 'nullable|image|mimes:jpeg,png,jpg|max:2048',
+    ] , [
+    'name.required' => 'กรุณากรอกชื่อสินค้า',
+    'description.required' => 'กรุณากรอกรายละเอียดสินค้า',
+    'price.required' => 'กรุณากรอกราคา',
+    'price.numeric' => 'ราคาต้องเป็นตัวเลข',
+    'image.image' => 'ไฟล์ต้องเป็นรูปภาพ',
+]
+);    
+
+    // ตรวจสอบว่ามีการอัปโหลดรูปภาพ
+    if ($request->hasFile('image')) {
+        $imagePath = $request->file('image')->store('uploads', 'public');
+        $url = Storage::url($imagePath);
+        $data["image"] =$url;
+    }
+
+    // บันทึกข้อมูลในฐานข้อมูล
+    Product::create($data);
+
+    return redirect()->route('product.index')->with('success', 'เพิ่มสินค้าแล้ว!');
+})->name('product.submit');
+
+use App\Http\Controllers\WeightController;
+
+
+Route::get('/', function () {
+    return redirect()->route('weights.index');
+});
+
+Route::resource('weights', WeightController::class);
